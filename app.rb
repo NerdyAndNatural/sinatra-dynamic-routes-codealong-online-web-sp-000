3f77cc1e16ec73daf438fcdfb5bf0b344b05e9cd
Sarah Williams <srwilliams8678@gmail.com>
2019-09-26 20:27:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
     end
     
     get "/multiply/:num1/:num2" do
-        @num3 = [:num1] * [:num2]
+        @num3 = params[:num1] * params[:num2]
         "#{@num3}"
     end
 end
