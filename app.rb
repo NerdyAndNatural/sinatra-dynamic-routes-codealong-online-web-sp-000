e124325e0a3e2c3e2b06c793d9f43d756f76d31e
Sarah Williams <srwilliams8678@gmail.com>
2019-09-26 20:29:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
       "Goodbye, #{@user_name}."
     end
     
-    get "/multiply/:num1/:num2" do
+    get '/multiply/:num1/:num2' do
         @new_num = params[:num1] * params[:num2]
         "#{@new_num}"
     end
